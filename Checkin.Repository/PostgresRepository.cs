dbe52a31cb89ae80efbbf0bc82e713fd5d48ae5c
wally.chang <wally.chang@protolabs.co.jp>
2018-10-15 17:41:35 +0900

    Adding skeleton

---
+++ Checkin.Repository/PostgresRepository.cs
@@ -0,0 +1,11 @@
+﻿using Checkin.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkin.Repository
+{
+	public class PostgresRepository : IRDBRepository
+	{
+	}
+}
---
+++ Checkin.Services/AuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using Checkin.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkin.Services
+{
+	public class AuthenticationService : IAuthenticationService
+	{
+	}
+}
